35bf7975551379dec445483eb58c1468543a149a
Camobrie <bvmeggelen@gmail.com>
2024-03-28 16:10:44 +0100

    do some magic voodoo stuff with type names

--- src/Types.hs
+++ src/Types.hs
@@ -16,7 +16,7 @@ import GHC.Generics
 type EName = Int
 
 -- | Entity is a list of components
-type Entity = [Component]
+type Entity = M.Map TypeRep Component
 
 -- | Component is a wrapper around some data
 data Component where
@@ -27,8 +27,7 @@ data ComponentData a where
   CD :: (Typeable a, Show a, Generic a) => a -> ComponentData a
 
 -- | Query is a wrapper around querying for a specific type of component
-data Query a where
-  Q :: (Typeable a, Show a, Generic a) => ComponentData a -> Query a
+type Query a = TypeRep
 
 instance Show Component where
   show (C c) = show c
@@ -38,8 +37,8 @@ instance Show (ComponentData a) where
 
 -- | Position component
 data Position = Position
-  { pos_x :: Double,
-    pos_y :: Double
+  { pos_x :: Int,
+    pos_y :: Int
   }
   deriving (Show, Eq, Data, Typeable, Generic)
 
@@ -60,7 +59,7 @@ printWorld :: World -> IO ()
 printWorld (World e) = do
   mapM_ printEntity (M.toList e)
 
-printEntity :: (EName, [Component]) -> IO ()
+printEntity :: (EName, Entity) -> IO ()
 printEntity (n, entity) = do
   putStrLn $ "\n-----Entity " ++ show n ++ "-----"
   mapM_ printComponent entity
@@ -69,19 +68,21 @@ printComponent :: Component -> IO ()
 printComponent (C (CD c)) = print c
 
 mkEntity :: Entity
-mkEntity = [C $ CD (Position 0 0), C $ CD (Name "Entity")]
+mkEntity = M.fromList [(typeOf Position, (C $ CD (Position 0 0))), (typeOf Name, (C $ CD (Name "Entity")))]
 
 mkEntity2 :: Entity
-mkEntity2 = [C $ CD (Position 1 1), C $ CD (Name "Entity2")]
+mkEntity2 = M.fromList [(typeOf Position, (C $ CD (Position 1 1))), (typeOf Name, (C $ CD (Name "Entity2")))]
 
 mkWorld :: World
 mkWorld = World $ M.fromList [(1, mkEntity), (2, mkEntity2)]
 
 move1 :: World -> World
-move1 = mapW (Q $ CD (Position 0 0)) (\(Position x y) -> Position (x + 1) y)
+move1 = mapW (typeOf Position) (\(Position x y) -> Position (x + 1) y)
 
-go :: World -> World
-go = runStep [move1, move1, move1]
+-- go :: World -> World
+-- go = runStep [move1, move1, move1]
+
+---------- QUERY FUNCTIONS ----------
 
 ---------- SYSTEM FUNCTIONS -------------
 mapW :: (Typeable a, Show a) => Query a -> (a -> a) -> World -> World
@@ -94,17 +95,13 @@ runStep (s : ss) w = runStep ss (s w)
 ---------- COMPONENT FUNCTIONS ----------
 modifyComponent :: (Typeable a) => Query a -> (a -> a) -> Entity -> Entity
 modifyComponent q f e = case lookupComponent q e of
-  Just (CD c) -> replaceComponent e (C $ CD (f c))
+  Just (CD c) -> replaceComponent e q (C $ CD (f c))
   Nothing -> e
 
 lookupComponent :: (Typeable a) => Query a -> Entity -> Maybe (ComponentData a)
-lookupComponent _ [] = Nothing
-lookupComponent (Q q) (C c : cs)
-  | typeOf c == typeOf q = cast c
-  | otherwise = lookupComponent (Q q) cs
+lookupComponent (t) e = case M.lookup t e of
+  Just (C c) -> cast c
+  Nothing -> Nothing
 
-replaceComponent :: Entity -> Component -> Entity
-replaceComponent [] _ = []
-replaceComponent (C c : cs) (C c')
-  | typeOf c == typeOf c' = C c' : cs
-  | otherwise = C c : replaceComponent cs (C c')
+replaceComponent :: Entity -> Query a -> Component -> Entity
+replaceComponent e q (C (CD c)) = M.insert q (C $ CD c) e
