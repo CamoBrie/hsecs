d5e7c1669dfc74756ff5db13f5e7665bec7f74f2
Camobrie <bvmeggelen@gmail.com>
2024-03-28 14:58:07 +0100

    initial ECS system

--- src/Types.hs
+++ src/Types.hs
@@ -10,21 +10,25 @@ module Types where
 
 import Data.Data
 import qualified Data.Map as M
-import Debug.Trace
 import GHC.Generics
 
+-- | Entity name
 type EName = Int
 
+-- | Entity is a list of components
 type Entity = [Component]
 
+-- | Component is a wrapper around some data
 data Component where
-  C :: (Typeable a, Show a) => ComponentData a -> Component
+  C :: (Typeable a, Show a, Generic a) => ComponentData a -> Component
 
+-- | ComponentData is the actual data stored in a component
 data ComponentData a where
-  CD :: (Typeable a, Show a) => a -> ComponentData a
+  CD :: (Typeable a, Show a, Generic a) => a -> ComponentData a
 
+-- | Query is a wrapper around querying for a specific type of component
 data Query a where
-  Q :: (Typeable a, Show a) => ComponentData a -> Query a
+  Q :: (Typeable a, Show a, Generic a) => ComponentData a -> Query a
 
 instance Show Component where
   show (C c) = show c
@@ -32,17 +36,20 @@ instance Show Component where
 instance Show (ComponentData a) where
   show (CD c) = show c
 
+-- | Position component
 data Position = Position
   { pos_x :: Double,
     pos_y :: Double
   }
   deriving (Show, Eq, Data, Typeable, Generic)
 
+-- | Name component
 data Name = Name
   { name :: String
   }
   deriving (Show, Eq, Data, Typeable, Generic)
 
+-- | World is a map of entities
 data World = World
   { entities :: M.Map EName Entity
   }
@@ -51,12 +58,11 @@ data World = World
 
 printWorld :: World -> IO ()
 printWorld (World e) = do
-  putStrLn "World:"
   mapM_ printEntity (M.toList e)
 
 printEntity :: (EName, [Component]) -> IO ()
 printEntity (n, entity) = do
-  putStrLn $ "Entity " ++ show n ++ ":"
+  putStrLn $ "\n-----Entity " ++ show n ++ "-----"
   mapM_ printComponent entity
 
 printComponent :: Component -> IO ()
@@ -72,19 +78,30 @@ mkWorld :: World
 mkWorld = World $ M.fromList [(1, mkEntity), (2, mkEntity2)]
 
 move1 :: World -> World
-move1 (World e) = World $ M.map (moveEntity 1) e
+move1 = mapW (Q $ CD (Position 0 0)) (\(Position x y) -> Position (x + 1) y)
 
-moveEntity :: Double -> Entity -> Entity
-moveEntity dx entity = case lookupComponent entity (Q $ CD (Position 0 0)) of
-  Just (CD (Position x y)) -> replaceComponent entity (C $ CD (Position (x + dx) y))
-  Nothing -> entity
+go :: World -> World
+go = runStep [move1, move1, move1]
+
+---------- SYSTEM FUNCTIONS -------------
+mapW :: (Typeable a, Show a) => Query a -> (a -> a) -> World -> World
+mapW q f (World e) = World $ M.map (modifyComponent q f) e
+
+runStep :: [World -> World] -> World -> World
+runStep [] w = w
+runStep (s : ss) w = runStep ss (s w)
 
 ---------- COMPONENT FUNCTIONS ----------
-lookupComponent :: (Typeable a) => Entity -> Query a -> Maybe (ComponentData a)
-lookupComponent [] _ = Nothing
-lookupComponent (C c : cs) (Q q)
+modifyComponent :: (Typeable a) => Query a -> (a -> a) -> Entity -> Entity
+modifyComponent q f e = case lookupComponent q e of
+  Just (CD c) -> replaceComponent e (C $ CD (f c))
+  Nothing -> e
+
+lookupComponent :: (Typeable a) => Query a -> Entity -> Maybe (ComponentData a)
+lookupComponent _ [] = Nothing
+lookupComponent (Q q) (C c : cs)
   | typeOf c == typeOf q = cast c
-  | otherwise = lookupComponent cs (Q q)
+  | otherwise = lookupComponent (Q q) cs
 
 replaceComponent :: Entity -> Component -> Entity
 replaceComponent [] _ = []
