aad2c270d550b74f862d619b6bdab8bac7b43b4d
Martijn <mdrenth01@gmail.com>
2024-04-12 22:25:02 +0200

    more tests

--- test/Main.hs
+++ test/Main.hs
@@ -23,6 +23,9 @@ tests =
     ,   testProperty "Double Component Result can create new components" doubleComponentEffectCreates
     ,   testProperty "Single Component Result updates if present" singleComponentEffectUpdates
     ,   testProperty "Double Component Result update if present" doubleComponentEffectUpdates
+    ,   testProperty "Not Queries those without the component" notOnlyMatchesThoseWithout
+    ,   testProperty "Not Queries those without the component, even when nothing matches" notCanMatchNothing
+    ,   testProperty "Tuple queries match and wise" queryAnd
     ]
 
 singleComponentEffectCreates :: Bool
@@ -37,8 +40,23 @@ singleComponentEffectUpdates = run [entAC1] (mapW $ \A -> C2) `equiv` [entAC2]
 doubleComponentEffectUpdates :: Bool
 doubleComponentEffectUpdates = run [entABTC1] (mapW $ \A -> (B False, C2)) `equiv` [entABFC2]
 
+notOnlyMatchesThoseWithout :: Bool
+notOnlyMatchesThoseWithout = run [entA, entABT] (mapW sys) `equiv` [entABF, entABT]
+    where sys :: Not B -> B
+          sys _ = B False
+
+notCanMatchNothing :: Bool
+notCanMatchNothing  = run [entABF, entABT] (mapW sys) `equiv` [entABF, entABT]
+    where sys :: Not B -> C
+          sys _ = C2
+
+queryAnd :: Bool
+queryAnd = run [entA, entABT] (mapW $ \(A, (B _)) -> C1) `equiv` [entA, entABTC1]
+
+
 entA = mkEntity >:> A
 entABT = mkEntity >:> A >:> B True
+entABF = mkEntity >:> A >:> B False
 entAC1 = mkEntity >:> A >:> C1
 entAC2 = mkEntity >:> A >:> C2
 entABTC1 = mkEntity >:> A >:> B True >:> C1
