cc034d32998150d354baf5376c5e6f3ea695b931
Camobrie <bvmeggelen@gmail.com>
2024-04-12 21:11:16 +0200

    add test suite

--- test/Main.hs
+++ test/Main.hs
@@ -1,13 +1,17 @@
 module Main (main) where
 
+import Data.Maybe (isJust)
+import Functions
+import Helpers
 import Test.Tasty
 import Test.Tasty.QuickCheck
+import Type.Reflection
 
 main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Main"
-    [
-    ]
-
+tests =
+  testGroup
+    "Main"
+    []
